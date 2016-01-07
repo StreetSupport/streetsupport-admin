524dc483d7d349d3e9978b6a612c73995a52d79b
Vince Lee <vslee888@gmail.com>
2016-01-07 11:36:48 +0000

    moved model into own module

---
+++ src/js/models/Login.js
@@ -0,0 +1,7 @@
+function LoginModel () {
+  this.username = 'vincey'
+  this.password = 'wangers'
+  this.message = 'hello world'
+}
+
+module.exports = LoginModel
--- src/js/pages/page-login.js
+++ src/js/pages/page-login.js
@@ -5,15 +5,8 @@ var nav = require('./../nav.js')
 nav.init()
 FastClick.attach(document.body)
 
-require.ensure(['knockout'], function (require) {
+require.ensure(['knockout', '../models/Login'], function (require) {
   var ko = require('knockout')
-  console.log(ko)
-
-  function LoginModel () {
-    this.username = 'vincey'
-    this.password = 'wangers'
-    this.message = 'hello'
-  }
-
-  ko.applyBindings(new LoginModel())
+  var model = require('../models/Login')
+  ko.applyBindings(new model())
 })
