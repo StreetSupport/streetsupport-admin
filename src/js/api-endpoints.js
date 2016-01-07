add09ca98bb19b07493ddd835f4d879bf2321900
Vince Lee <vslee888@gmail.com>
2016-01-07 20:34:26 +0000

    point to staging

--- src/js/api-endpoints.js
+++ src/js/api-endpoints.js
@@ -2,7 +2,7 @@ var dev = 'http://localhost:55881' // eslint-disable-line
 var staging = 'http://streetsupport-api-staging.apphb.com' // eslint-disable-line
 var live = 'http://api.streetsupport.net' // eslint-disable-line
 
-var domainRoot = dev
+var domainRoot = staging
 
 var createSessionUrl = '/v1/sessions/create'
 
