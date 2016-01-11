ba7d5b547a604aee0ddae7c5da3d9c23fb771139
Vince Lee <vslee888@gmail.com>
2016-01-11 09:11:22 +0000

    point to dev

--- src/js/api-endpoints.js
+++ src/js/api-endpoints.js
@@ -3,7 +3,7 @@ var staging = 'http://streetsupport-api-staging.apphb.com' // eslint-disable-lin
 var live = 'http://api.streetsupport.net' // eslint-disable-line
 
 function p (url) {
-  return staging + url
+  return dev + url
 }
 
 var createSessionUrl = '/v1/sessions/create'
