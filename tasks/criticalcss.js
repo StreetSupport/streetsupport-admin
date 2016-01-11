5f0c3b7e2cae9f489eaa05b252a1ccc283c37d70
Vince Lee <vslee888@gmail.com>
2016-01-11 23:04:27 +0000

    disable criticalcss

--- tasks/criticalcss.js
+++ tasks/criticalcss.js
@@ -10,16 +10,16 @@ var gulpif  = require('gulp-if');
 var critical = require('critical').stream;
 
 // HTML minify task
-gulp.task('crticalcss', function () {
-  return gulp.src(config.paths.build + '/*.html')
-    .pipe(gulpif(argv.debug === true, debug({title: 'CSS Inlined:'})))
-    .pipe(critical({
-      base: config.paths.build + '/',
-      width: 320,
-      height: 480,
-      minify: true,
-      extract: false,
-      inline: true
-    }))
-    .pipe(gulp.dest(config.paths.build));
-});
+// gulp.task('crticalcss', function () {
+//   return gulp.src(config.paths.build + '/*.html')
+//     .pipe(gulpif(argv.debug === true, debug({title: 'CSS Inlined:'})))
+//     .pipe(critical({
+//       base: config.paths.build + '/',
+//       width: 320,
+//       height: 480,
+//       minify: true,
+//       extract: false,
+//       inline: true
+//     }))
+//     .pipe(gulp.dest(config.paths.build));
+// });
