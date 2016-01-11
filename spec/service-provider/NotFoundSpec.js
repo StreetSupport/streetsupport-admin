89fdcfedd7ee6abc28bb2da65e6b715890860e8d
Vince Lee <vslee888@gmail.com>
2016-01-11 11:11:58 +0000

    if 404, redirect to 404

---
+++ spec/service-provider/NotFoundSpec.js
@@ -0,0 +1,48 @@
+var sinon = require('sinon'),
+    ajax =      require('basic-ajax'),
+    endpoints = require('../../src/js/api-endpoints'),
+    adminurls = require('../../src/js/admin-urls'),
+    browser =   require('../../src/js/browser'),
+    cookies =   require('../../src/js/cookies'),
+    getUrlParameter = require('../../src/js/get-url-parameter')
+
+
+describe('Dashboard', function () {
+  var Model = require('../../src/js/models/ServiceProvider'),
+  model,
+  stubbedApi,
+  stubbedCookies,
+  stubbedUrlParams
+
+  beforeEach(function () {
+    function fakeResolved(value) {
+      return {
+        then: function(success, error) {
+          success({
+            'status': 404,
+            'json': {}
+          })
+        }
+      }
+    }
+
+    stubbedApi = sinon.stub(ajax, 'get').returns(fakeResolved())
+    stubbedCookies = sinon.stub(cookies, 'get').returns('stored-session-token')
+    stubbedUrlParams = sinon.stub(getUrlParameter, 'parameter').returns('coffee4craig')
+    stubbedBrowser = sinon.stub(browser, 'redirect')
+
+    model = new Model()
+  })
+
+  afterEach(function () {
+    ajax.get.restore()
+    cookies.get.restore()
+    getUrlParameter.parameter.restore()
+    browser.redirect.restore()
+  })
+
+  it('should redirect browser to dashboard', function() {
+    var browserRedirectedWithExpectedUrl = stubbedBrowser.withArgs(adminurls.dashboard).calledOnce
+    expect(browserRedirectedWithExpectedUrl).toBeTruthy()
+  })
+})
--- src/js/models/ServiceProvider.js
+++ src/js/models/ServiceProvider.js
@@ -2,6 +2,7 @@ var ajax = require('basic-ajax')
 var endpoints = require('../api-endpoints')
 var adminUrls = require('../admin-urls')
 var cookies = require('../cookies')
+var browser = require('../browser')
 var getUrlParameter = require('../get-url-parameter')
 var ko = require('knockout')
 var _ = require('lodash')
@@ -18,7 +19,8 @@ function ServiceProvider () {
     },
     {})
   .then(function (result) {
-    self.serviceProvider = result.json
+    if(result.status === 200) self.serviceProvider = result.json
+    if(result.status === 404) browser.redirect(adminUrls.dashboard)
   },
   function (error) {
     alert('oops, there was a problem! ' + JSON.parse(error))
