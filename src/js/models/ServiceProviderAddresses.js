4700d1bb8e47b46400d0487d8da0b471ae0f26b0
Vince Lee <vslee888@gmail.com>
2016-01-12 15:38:51 +0000

    wire up view

--- src/js/models/ServiceProviderAddresses.js
+++ src/js/models/ServiceProviderAddresses.js
@@ -4,11 +4,39 @@ var adminUrls = require('../admin-urls')
 var cookies = require('../cookies')
 // var browser = require('../browser')
 var getUrlParameter = require('../get-url-parameter')
-// var ko = require('knockout')
-// var _ = require('lodash')
+var ko = require('knockout')
+var _ = require('lodash')
+
+function OpeningTime (data) {
+  this.day = data.day
+  this.startTime = data.startTime
+  this.endTime = data.endTime
+}
+
+function Address (data) {
+  this.street1 = ko.observable(data.street)
+  this.street2 = ko.observable(data.street1)
+  this.street3 = ko.observable(data.street2)
+  this.street4 = ko.observable(data.street3)
+  this.city = ko.observable(data.city)
+  this.postcode = ko.observable(data.postcode)
+
+  this.openingTimes = ko.observableArray(_.map(data.openingTimes, function (time) {
+    return new OpeningTime(time)
+  }))
+}
+
+function ServiceProvider (data) {
+  this.key = ko.observable(data.key)
+  this.name = ko.observable(data.name)
+  this.addresses = ko.observableArray(_.map(data.addresses, function (address) {
+    return new Address(address)
+  }))
+}
 
 function ServiceProviderAddresses () {
   var self = this
+  self.serviceProvider = ko.observable()
 
   self.init = function () {
     ajax.get(endpoints.serviceProviderAddresses + '/show/' + getUrlParameter.parameter('key'),
@@ -18,12 +46,13 @@ function ServiceProviderAddresses () {
       },
       {})
       .then(function (result) {
+        self.serviceProvider(new ServiceProvider(result.json))
       },
       function (error) {
       })
   }
 
-  self.init ()
+  self.init()
 }
 
 module.exports = ServiceProviderAddresses
