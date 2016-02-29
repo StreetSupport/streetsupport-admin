3dd9c4d360eaff86e85144effa4e42458853e1d9
Vince Lee <vslee888@gmail.com>
2016-02-29 16:08:06 +0000

    revert again

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -41,6 +41,9 @@ cd ../../
 # Run gulp
 gulp deploy --debug --production
 
+# Move to created directory
+cd _dist
+
 echo "pushing to repo"
 
 # Push to git by overriding previous commits
@@ -48,28 +51,18 @@ echo "pushing to repo"
 
 if [[ $TRAVIS_BRANCH == 'master' ]] # live
   then
-    cd _dist
     git init
     git add -A
     git commit -m "Travis CI automatic build for $THE_COMMIT"
     git push --force --quiet "https://${GH_TOKEN}@${REPO}" master > /dev/null 2>&1
 fi
 
+
 if [[ $TRAVIS_BRANCH == 'develop' ]] # dev
   then
-    openssl aes-256-cbc -K $encrypted_516b0b657008_key -iv $encrypted_516b0b657008_iv -in travis-deploy-key.enc -out .\\travis-deploy-key -d
-    rm travis-deploy-key.enc # Don't need it anymore
-    chmod 600 travis-deploy-key
-    mv travis-deploy-key ~/.ssh/id_rsa
-
-    cd _dist
     git init
-
-    git remote add deploy "travisdeploy@178.62.41.238:/usr/share/nginx/html/admin-staging.streetsupport.net"
-    git config user.name "Travis CI"
-    git config user.email "support@streetsupport.net"
-
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
+    git add -A
+    git commit -m "Travis CI automatic build for $THE_COMMIT"
+    git push --force --quiet "https://${GH_TOKEN}@${REPO}" master:gh-pages > /dev/null 2>&1
+    echo $vincetestenvvar
 fi
