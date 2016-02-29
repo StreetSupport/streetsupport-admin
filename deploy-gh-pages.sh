bc2fb236d7005d047ed997496bc978141f6b47ed
Vince Lee <vslee888@gmail.com>
2016-02-29 11:30:11 +0000

    remove commented CNAME creation

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -44,13 +44,6 @@ gulp deploy --debug --production
 # Move to created directory
 cd _dist
 
-echo "creating cname file"
-
-# Create CNAME file and populate with domain depending on branch
-#cat > CNAME << EOF
-#$DOMAIN
-#EOF
-
 echo "pushing to repo"
 
 # Push to git by overriding previous commits
