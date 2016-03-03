eea21f1d76251e90532ec82416df92766877c265
Vince Lee <vslee888@gmail.com>
2016-03-03 11:52:13 +0000

    edit provider short description. clear errors on successful submission

--- src/js/models/ServiceProvider.js
+++ src/js/models/ServiceProvider.js
@@ -15,6 +15,7 @@ function ServiceProvider (data) {
 
   self.key = ko.observable(data.key)
   self.name = ko.observable(data.name)
+  self.shortDescription = ko.observable(data.shortDescription)
   self.description = ko.observable(data.description)
   self.telephone = ko.observable(data.telephone)
   self.email = ko.observable(data.email)
@@ -110,13 +111,14 @@ function ServiceProviderDetails () {
       ajax.put(self.endpointBuilder.serviceProviders(getUrlParameter.parameter('key')).generalInformation().build(),
         self.headers(cookies.get('session-token')),
         JSON.stringify({
-          'Description': self.serviceProvider().description()
+          'Description': self.serviceProvider().description(),
+          'ShortDescription': self.serviceProvider().shortDescription()
         })
         ).then(function (result) {
+          self.clearErrors()
           self.isEditingGeneralDetails(false)
         }, function (error) {
-          var response = JSON.parse(error.response)
-          self.message(response.messages.join('<br />'))
+          self.setErrors(error)
         })
     }
   }
