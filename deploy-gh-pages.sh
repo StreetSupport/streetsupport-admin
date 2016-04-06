4409984876e500c730f48310e7cdf3603d48b831
Vince Lee <vslee888@gmail.com>
2016-04-06 10:42:56 +0100

    meh

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -7,23 +7,14 @@ set -e
 if [[ $TRAVIS_BRANCH == 'release' ]]
   then
     APIENVIRONMENT=3
-    AZURE_USER=$LIVE_AZURE_USER
-    AZURE_PASSWORD=$LIVE_AZURE_PASSWORD
-    AZURE_WEBSITE=$LIVE_AZURE_WEBSITE
 fi
 if [[ $TRAVIS_BRANCH == 'staging' ]]
   then
     APIENVIRONMENT=2
-    AZURE_USER=$STAGING_AZURE_USER
-    AZURE_PASSWORD=$STAGING_AZURE_PASSWORD
-    AZURE_WEBSITE=$STAGING_AZURE_WEBSITE
 fi
 if [[ $TRAVIS_BRANCH == 'develop' ]]
   then
     APIENVIRONMENT=1
-    AZURE_USER=$DEV_AZURE_USER
-    AZURE_PASSWORD=$DEV_AZURE_PASSWORD
-    AZURE_WEBSITE=$DEV_AZURE_WEBSITE
 fi
 
 # Get the commit details
@@ -60,18 +51,29 @@ cd _dist
 
 echo "pushing to repo"
 
-echo $AZURE_USER
-echo $AZURE_WEBSITE
-
 # Push to git by overriding previous commits
 # IMPORTANT: Supress messages so nothing appears in logs
 
-if [[ $TRAVIS_BRANCH == 'release' ]] || [[ $TRAVIS_BRANCH == 'staging' ]] || [[ $TRAVIS_BRANCH == 'develop' ]]
+if [[ $TRAVIS_BRANCH == 'release' ]]
   then
     git init
     git add -A
     git commit -m "Travis CI automatic build for $THE_COMMIT"
-    git push --quiet --force "https://${AZURE_USER}:${AZURE_PASSWORD}@${AZURE_WEBSITE}.scm.azurewebsites.net:443/${AZURE_WEBSITE}.git" master > /dev/null 2>&1
-  else
-    echo "Not on a build branch so don't push"
+    git push --quiet --force "https://${LIVE_AZURE_USER}:${LIVE_AZURE_PASSWORD}@${LIVE_AZURE_WEBSITE}.scm.azurewebsites.net:443/${LIVE_AZURE_WEBSITE}.git" master > /dev/null 2>&1
+fi
+
+if [[ $TRAVIS_BRANCH == 'staging' ]]
+  then
+    git init
+    git add -A
+    git commit -m "Travis CI automatic build for $THE_COMMIT"
+    git push --quiet --force "https://${STAGING_AZURE_USER}:${STAGING_AZURE_PASSWORD}@${STAGING_AZURE_WEBSITE}.scm.azurewebsites.net:443/${STAGING_AZURE_WEBSITE}.git" master > /dev/null 2>&1
+fi
+
+if [[ $TRAVIS_BRANCH == 'develop' ]]
+  then
+    git init
+    git add -A
+    git commit -m "Travis CI automatic build for $THE_COMMIT"
+    git push --quiet --force "https://${DEV_AZURE_USER}:${DEV_AZURE_PASSWORD}@${DEV_AZURE_WEBSITE}.scm.azurewebsites.net:443/${DEV_AZURE_WEBSITE}.git" master > /dev/null 2>&1
 fi
