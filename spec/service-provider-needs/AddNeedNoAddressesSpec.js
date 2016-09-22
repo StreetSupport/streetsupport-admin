a6294b7bbc5ca6789582df0484c077ce890dd60d
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2016-09-22 15:34:48 +0100

    test around no addresses when adding need

---
+++ spec/service-provider-needs/AddNeedNoAddressesSpec.js
@@ -0,0 +1,53 @@
+/*
+global describe, beforeEach, afterEach, it, expect
+*/
+
+'use strict'
+
+var sinon = require('sinon')
+var ajax = require('../../src/js/ajax')
+var endpoints = require('../../src/js/api-endpoints')
+var browser = require('../../src/js/browser')
+var cookies = require('../../src/js/cookies')
+var getUrlParameter = require('../../src/js/get-url-parameter')
+
+describe('Add individual Need with no addresses', () => {
+  var Model = require('../../src/js/models/service-provider-needs/AddServiceProviderNeed')
+  var model
+
+  beforeEach(() => {
+    sinon.stub(browser, 'loading')
+    sinon.stub(browser, 'loaded')
+    sinon.stub(getUrlParameter, 'parameter').withArgs('providerId').returns('coffee4craig')
+
+    let fakeGetResolution = {
+      then: function (success, error) {
+        success({
+          'statusCode': 200,
+          'data': []
+        })
+      }
+    }
+    sinon.stub(cookies, 'get').returns('saved-session-token')
+    sinon.stub(ajax, 'get').withArgs(
+      endpoints.getServiceProviders + '/coffee4craig/addresses',
+      {
+        'content-type': 'application/json',
+        'session-token': 'saved-session-token'
+      }
+    ).returns(fakeGetResolution)
+    model = new Model()
+  })
+
+  afterEach(() => {
+    browser.loading.restore()
+    browser.loaded.restore()
+    getUrlParameter.parameter.restore()
+    ajax.get.restore()
+    cookies.get.restore()
+  })
+
+  it('should set postcode to empty string', () => {
+    expect(model.need().postcode()).toEqual('')
+  })
+})
