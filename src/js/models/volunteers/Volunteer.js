5a34a4c4b54825c56dd31c6363c79da8f2c7c216
Vince Lee <vslee888@gmail.com>
2016-11-16 14:16:57 +0000

    view history

--- src/js/models/volunteers/Volunteer.js
+++ src/js/models/volunteers/Volunteer.js
@@ -1,6 +1,12 @@
 'use strict'
 
 const adminUrls = require('../../admin-urls')
+const ajax = require('../../ajax')
+const browser = require('../../browser')
+const cookies = require('../../cookies')
+const endpoints = require('../../api-endpoints')
+const htmlEncode = require('htmlencode')
+const BaseViewModel = require('../../models/BaseViewModel')
 const moment = require('moment')
 const ko = require('knockout')
 
@@ -33,6 +39,35 @@ const Volunteer = function (data) {
       ? 'volunteer volunteer--highlighted'
       : 'volunteer'
   }, self)
+
+  self.contactHistory = ko.observableArray()
+  self.hasContactHistory = ko.observable(false)
+  self.hasRetrievedContactHistory = ko.observable(false)
+
+  self.getContactHistory = () => {
+    browser.loading()
+
+    ajax
+      .get(
+        endpoints.volunteers + '/' + self.id + '/contact-requests',
+        self.headers(cookies.get('session-token')),
+        {})
+      .then((result) => {
+        let items = result.data.embedded.items
+        items.forEach((i) => {
+          i.message = htmlEncode.htmlDecode(i.message)
+          i.createdDate = moment(i.createdDate).format('hh:mm DD/MM/YY')
+        })
+        self.contactHistory(result.data.embedded.items)
+        self.hasContactHistory(result.data.embedded.items.length > 0)
+        self.hasRetrievedContactHistory(true)
+        browser.loaded()
+      }, (_) => {
+        browser.redirect(adminUrls.fiveHundred)
+      })
+  }
 }
+
+Volunteer.prototype = new BaseViewModel()
 
 module.exports = Volunteer
