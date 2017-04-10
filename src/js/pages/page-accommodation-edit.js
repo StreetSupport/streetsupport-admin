ef7a1b79322ffeba4e973793b6be63296686e3ad
Vince Lee <vslee888@gmail.com>
2017-04-10 21:13:56 +0100

    remove redundant webpack item

--- src/js/pages/page-accommodation-edit.js
+++
@@ -1,8 +0,0 @@
-import '../common'
-require.ensure(['knockout', '../models/accommodation/edit'], (require) => {
-  const ko = require('knockout')
-  const Model = require('../models/accommodation/edit')
-  const model = new Model()
-  model.init()
-  ko.applyBindings(model)
-})
