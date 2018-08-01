d5aa277a860938e9c412b7293462ba19e3bf9657
Vince Lee <vslee888@gmail.com>
2018-08-01 10:36:40 +0100

    adding accom - sort SPs by name

--- src/js/models/accommodation/add.js
+++ src/js/models/accommodation/add.js
@@ -66,7 +66,13 @@ function Model () {
                 key: p.key,
                 name: htmlEncode.htmlDecode(p.name)
               }
-            }))
+            })
+            .sort((a, b) => {
+              if (a.name > b.name) return 1
+              if (a.name < b.name) return -1
+              return 0
+            })
+          )
         }, () => {
           self.handleServerError()
         })
